444303da0eb96f71b1bb21c3a269271c8329476d
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-10-29 14:54:41 +1000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,10 +7,11 @@ kind create cluster --config .devcontainer/kind-cluster.yml --wait 5m
 # TODO
 
 # Creation Ping
-curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
-  -H "Content-Type: application/json" \
-  -d "{
-    \"repo\": \"$GITHUB_REPOSITORY\",
-    \"demo\": \"demo-PLACEHOLDER\",
-    \"codespace.name\": \"$CODESPACE_NAME\"
-  }"
+# TODO: Uncomment when you're ready to go live
+# curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+#   -H "Content-Type: application/json" \
+#   -d "{
+#     \"repo\": \"$GITHUB_REPOSITORY\",
+#     \"demo\": \"demo-PLACEHOLDER\",
+#     \"codespace.name\": \"$CODESPACE_NAME\"
+#   }"
