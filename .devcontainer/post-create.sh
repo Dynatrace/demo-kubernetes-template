f0d58243941035dec38fd65c07a9fa682b7a6ad9
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-10-29 13:53:27 +1000

    Create post-create.sh

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create k8s cluster
+kind create cluster --config .devcontainer/kind-cluster.yml --wait 5m
+
+# Your content here
+# TODO
+
+# Creation Ping
+curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"repo\": \"$GITHUB_REPOSITORY\",
+    \"demo\": \"demo-PLACEHOLDER\",
+    \"codespace.name\": \"$CODESPACE_NAME\"
+  }"
