07407f4178b2238af11e2f7e7e1861a3fee9be6b
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2025-01-03 12:37:59 +1000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,7 +7,7 @@ kind create cluster --config .devcontainer/kind-cluster.yml --wait 5m
 # TODO
 
 # Creation Ping
-# TODO: Uncomment when you're ready to go live
+# TODO: Uncomment and update the PLACEHOLDER when you're ready to go live
 # curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
 #   -H "Content-Type: application/json" \
 #   -d "{
